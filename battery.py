261da7ecdd92a50f91e0c0720691752b6442161c
respasha <respasha@yandex.ru>
2016-05-07 16:55:47 +0300

    Various enhancements
    1. Changed battery colors. Now it goes from bright green at 100% to yellow at 50% and red at 10%.
    2. Added urgent exit status if percent value is below 5.
    3. Added onclick notification with status and time left.
    4. Changed icons.
    
    Now the blocklet is always same width.
    
    New dependency: notify-send

--- battery.py
+++ battery.py
@@ -2,27 +2,12 @@
 #
 # A battery indicator blocklet script for i3blocks
 
-from subprocess import check_output
-
-status = check_output(['acpi'], universal_newlines=True)
-state = status.split(": ")[1].split(", ")[0]
-commasplitstatus = status.split(", ")
-percentleft = int(commasplitstatus[1].rstrip("%\n"))
-
-FA_LIGHTNING = "<span font='FontAwesome'>\uf0e7</span>"
-FA_PLUG = "<span font='FontAwesome'>\uf1e6</span>"
-
-fulltext = "<span color='yellow'>{}</span>".format(FA_LIGHTNING)
-timeleft = ""
-
-if state == "Discharging":
-    time = commasplitstatus[-1].split()[0]
-    time = ":".join(time.split(":")[0:2])
-    timeleft = " ({})".format(time)
-else:
-    fulltext += " " + FA_PLUG
+from subprocess import check_output, Popen
+import os
 
 def color(percent):
+    if percent < 5:
+        return "#FFFFFF"
     if percent < 10:
         return "#FF0000"
     if percent < 20:
@@ -36,14 +21,46 @@ def color(percent):
     if percent < 60:
         return "#FFFF00"
     if percent < 70:
-        return "#FFFF33"
+        return "#99FF00"
     if percent < 80:
-        return "#FFFF66"
-    return "#FFFFFF"
+        return "#66FF00"
+    return "#33FF00"
 
-form =  ' <span color="{}">{}%</span>'
+status = check_output(['acpi'], universal_newlines=True)
+state = status.split(": ")[1].split(", ")[0]
+commasplitstatus = status.split(", ")
+percentleft = int(commasplitstatus[1].rstrip("%\n"))
+
+FA_CHR = "<span font='FontAwesome'>\uf077</span>"
+FA_DIS = "<span font='FontAwesome'>\uf078</span>"
+FA_FULL = "<span font='FontAwesome'>\uf139</span>"
+FA_LIGHTNING = "<span font='FontAwesome'>\uf0e7</span>"
+
+fulltext = "<span color='yellow'>{}</span>".format(FA_LIGHTNING)
+timeleft = state + ", time left:"
+time = commasplitstatus[-1].split()[0]
+time = ":".join(time.split(":")[0:2])
+timeleft += " {}".format(time)
+
+if state == "Discharging":
+    form = ' <span color="{}">{}</span>'
+    fulltext += form.format(color(percentleft), FA_DIS)
+elif state == "Charging":
+    fulltext += " " + FA_CHR
+else:
+    fulltext += " " + FA_FULL
+
+if percentleft != 100:
+    fulltext += ' '
+if percentleft < 10:
+    fulltext += ' '
+form =  '<span color="{}">{}%</span>'
 fulltext += form.format(color(percentleft), percentleft)
-fulltext += timeleft
+
+if os.environ.get('BLOCK_BUTTON'):
+    Popen(['notify-send', timeleft])
 
 print(fulltext)
 print(fulltext)
+if percentleft < 5:
+    exit(33)
