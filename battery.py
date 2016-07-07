16190bd0434c245aeed902ccebed04dda0b800fb
James Murphy <james.murphy.debian@gmail.com>
2016-07-07 15:35:56 +0000

    be consistent about what symbols mean

--- battery.py
+++ battery.py
@@ -7,28 +7,37 @@ from subprocess import check_output
 status = check_output(['acpi'], universal_newlines=True)
 
 if not status:
+    # stands for no battery found
     fulltext = "<span color='red'><span font='FontAwesome'>\uf00d \uf240</span></span>"
 else:
     state = status.split(": ")[1].split(", ")[0]
     commasplitstatus = status.split(", ")
     percentleft = int(commasplitstatus[1].rstrip("%\n"))
 
-    FA_LIGHTNING = "<span font='FontAwesome'>\uf0e7</span>"
+    # stands for charging
+    FA_LIGHTNING = "<span color='yellow'><span font='FontAwesome'>\uf0e7</span></span>"
+
+    # stands for plugged in
     FA_PLUG = "<span font='FontAwesome'>\uf1e6</span>"
 
-    fulltext = "<span color='yellow'>{}</span>".format(FA_LIGHTNING)
+    fulltext = ""
     timeleft = ""
 
     if state == "Discharging":
         time = commasplitstatus[-1].split()[0]
         time = ":".join(time.split(":")[0:2])
         timeleft = " ({})".format(time)
+    elif state == "Full":
+        fulltext = FA_PLUG + " "
+    elif state == "Unknown":
+        fulltext = "<span font='FontAwesome'>\uf128</span> "
     else:
-        fulltext += " " + FA_PLUG
+        fulltext = FA_LIGHTNING + " " + FA_PLUG + " "
 
     def color(percent):
         if percent < 10:
-            return "#FF0000"
+            # exit code 33 will turn background red
+            return "#FFFFFF"
         if percent < 20:
             return "#FF3300"
         if percent < 30:
@@ -45,9 +54,11 @@ else:
             return "#FFFF66"
         return "#FFFFFF"
 
-    form =  ' <span color="{}">{}%</span>'
+    form =  '<span color="{}">{}%</span>'
     fulltext += form.format(color(percentleft), percentleft)
     fulltext += timeleft
 
 print(fulltext)
 print(fulltext)
+if percentleft < 10:
+    exit(33)
